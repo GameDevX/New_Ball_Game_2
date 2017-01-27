26885abf79056a698ab556a53504f757a33a9ae2
Thetudors <152804004@ogr.cbu.edu.tr>
2017-01-28 00:13:38 +0200

    First Commit
    First Commit

---
+++ Unity_Ball_Game/Assets/Scripts/Ball_Move.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball_Move : MonoBehaviour {
+
+    bool isContact;
+    Rigidbody Ball_rb;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        Ball_rb = gameObject.GetComponent<Rigidbody>();
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void FixedUpdate()
+    {
+        
+
+        Ball_Movement(10);
+        Ball_Jump(500);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            isContact = true;
+            
+        }
+      
+    }
+
+
+    void Ball_Jump(float Jump_Speed)
+    {
+        if (isContact)
+        {
+            Ball_rb.AddForce(new Vector3(0, Jump_Speed));
+            isContact = false;
+        }
+    }
+    
+    void Ball_Movement(float speed)
+    {
+       
+        float Move_Horizontal;
+        Move_Horizontal = Input.GetAxis("Horizontal");
+        Ball_rb.AddForce(new Vector3(Move_Horizontal * speed, 0, 0));
+    }
+
+    
+
+}
---
+++ Unity_Ball_Game/Assets/Scripts/Cam_Follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam_Follow : MonoBehaviour {
+
+   
+    public Transform Target;
+    
+    Vector3 offset = new Vector3(0, 0, -10);
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        Vector3 desiredPosition = new Vector3(Target.transform.position.x,0,0) + offset;
+        transform.position = desiredPosition;
+	
+	}
+}
